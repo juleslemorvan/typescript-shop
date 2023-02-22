620c5e537ddfdfed50eb4b32a2c5efe9b6e5ab15
jules <jules.lemorvan01@gmail.com>
2023-02-22 18:50:13 +0100

    creation de card dans la page store avec modele

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem"
+import dataItems from "../data/items.json"
 
 const Store = () => {
     return (
+        <>
        <h1>Store</h1>
+       <Row xs={1} md={2} lg={3} className="g-3" >
+        {dataItems.map(item => (
+            <Col key={item.id}><StoreItem {...item}/></Col>
+        ))}
+       </Row>
+
+       </>
     );
 };
 
