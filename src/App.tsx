9411a3bc5c6c4c81ddece4dc278df6f167b9ef10
jules <jules.lemorvan01@gmail.com>
2023-03-02 16:59:41 +0100

    creation du context provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import  Home from "./pages/Home"
 import Store from "./pages/Store"
 import About from "./pages/About"
 import Navbar from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
     <Navbar/>
     <Container className="mb-4">
     <Routes>
@@ -19,7 +20,7 @@ function App() {
     </Routes>
 
     </Container>
-    </>
+    </ShoppingCartProvider>
   )
   
 }
